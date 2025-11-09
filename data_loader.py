13ee9156a46ea5fe632db05efcc6dfd314833fa3
Paul Cornelissen <paul@bit-byters.net>
2025-11-09 15:35:33 +0100

    add uv and fix bit rot bogs

--- data_loader.py
+++ data_loader.py
@@ -18,5 +18,13 @@ def df_by_src_year(dso: DSO, year: int = 2022, separator: str = "\t") -> pd.Data
     """
     Get a DataFrame for a given DSO and
     """
-    path: str = path_from(path_from("data", dso), f"{dso}-{year}.csv")
-    return pd.read_csv(path, sep=separator, engine="python", decimal=",", error_bad_lines=False, warn_bad_lines=True)
+    dso_name = dso.value
+    path: str = path_from(path_from("data", dso_name), f"{dso_name}-{year}.csv")
+    return pd.read_csv(
+        path,
+        sep=separator,
+        engine="python",
+        decimal=",",
+        on_bad_lines="warn",
+        encoding_errors="ignore",
+    )
